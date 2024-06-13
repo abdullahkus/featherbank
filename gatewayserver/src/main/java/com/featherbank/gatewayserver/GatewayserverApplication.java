81b476adaf062d4efb4da0aa452ae211c250c447
Abdullah KUŞ <70553662+abdullahkus@users.noreply.github.com>
2024-06-13 15:34:24 +0300

    gateway server was installed, accounts, loans and cards were integrated to gateway server, zipkin was installed and integrated to services.

---
+++ gatewayserver/src/main/java/com/featherbank/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,17 @@
+package com.featherbank.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+}
